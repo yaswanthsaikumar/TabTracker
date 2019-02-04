7f94281b26903c275f552348579fa6d32f72e629
Yaswanth <yskchowdary4@gmail.com>
2019-02-04 09:49:49 -0500

    5th commit

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input type="text" name="email" v-model="email" placeholder="email">
+    <br>
+      <input type="text" name="password" v-model="password" placeholder="password">
+      <br>
+      <button>Register</button>
+
   </div>
 </template>
 
@@ -8,7 +14,20 @@
 export default {
   data () {
     return {
+      email: 'abc',
+      password: '123'
+
     }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+
+    }, 1000)
   }
 }
 </script>
