1853346f1619bf18274cb3d6c222f70d47eb0540
Yaswanth <yskchowdary4@gmail.com>
2019-02-05 17:12:51 -0500

    commit 1st part

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const express= require('express')
 const bodyparser= require('body-parser')
 const cors= require('cors')
 const morgan= require('morgan')
-
 const app=express()
 app.use(morgan('combined'))
 app.use(bodyparser.json())
